021e5aaffaee671b033a89e0ca9f06c0b3cc0b8b
Thomas Guan <thomas@NASA-PC.>
2025-06-09 21:20:20 +1000

    cleaned up file

--- src/App.tsx
+++ src/App.tsx
@@ -9,133 +9,68 @@ import { Word } from "./components/Word";
 import { Score, Lives, Hints } from "./components/GameStat";
 import { GameReset } from "./components/GameReset";
 
+import {
+  handleClickAdd,
+  handleClickRemove,
+  handleClickHint,
+  handleGameReset,
+  handleClickCloseDictionary,
+  handleClickHome,
+  handleClickStartNorm,
+  handleClickStartHard,
+} from "./EventHandler";
+
 import { useState } from "react";
 import { generate } from "random-words";
 
-interface Game {
+export interface Game {
   score: number;
   lives: number;
   hints: number;
   dictionary: Map<string, string>;
 }
 
-const currGame: Game = {
+export const currGame: Game = {
   score: 0,
   lives: 3,
   hints: 5,
   dictionary: new Map(),
 };
 
-function App() {
+export function App() {
   // States for rendering
   const [home, setHome] = useState(true);
   const [hardMode, setHardMode] = useState(false);
-  const [score, setScore] = useState(currGame.score);
-  const [lives, setLives] = useState(currGame.lives);
-  const [hints, setHints] = useState(currGame.hints);
+  const [score, setNumScore] = useState(currGame.score);
+  const [lives, setNumLives] = useState(currGame.lives);
+  const [hints, setNumHints] = useState(currGame.hints);
   const [viewHints, setViewHints] = useState(false);
   const [word, setWord] = useState(() => generate({ maxLength: 2 }) as string);
   const [activeGame, setActiveGame] = useState(true);
 
-  const handleClickStartNorm = () => {
-    setHome(false);
-  };
-
-  const handleClickStartHard = () => {
-    currGame.lives = 1;
-    setLives(currGame.lives);
-    setHome(false);
-    setHardMode(true);
-  };
-
-  // When add is pressed
-  const handleClickAdd = () => {
-    // using currWord for race conditions, idk if this works tho
-    const currWord = word;
-    if (!currGame.dictionary.get(currWord)) {
-      currGame.score++;
-      setScore(currGame.score);
-      currGame.dictionary.set(currWord, currWord);
-    } else {
-      currGame.lives--;
-      setLives(currGame.lives);
-
-      // Condition for game over
-      currGame.lives <= 0 ? setActiveGame(false) : null;
-    }
-
-    const newWord = generate({ maxLength: 2 }) as string;
-    setWord(newWord);
-  };
-
-  // When removed is pressed
-  const handleClickRemove = () => {
-    const currWord = word;
-    if (currGame.dictionary.get(word)) {
-      currGame.score++;
-      setScore(currGame.score);
-      currGame.dictionary.set(currWord, currWord);
-    } else {
-      currGame.lives--;
-      setLives(currGame.lives);
-
-      // Condition for game over
-      currGame.lives <= 0 ? setActiveGame(false) : null;
-    }
-
-    const newWord = generate({ maxLength: 2 }) as string;
-    setWord(newWord);
-  };
-
-  // When hint is pressed
-  const handleClickHint = () => {
-    // console.log for testing
-    currGame.dictionary.forEach((key, value) => {
-      console.log(`${key}: ${value}`);
-    });
-    if (currGame.hints > 0) {
-      currGame.hints--;
-      setViewHints(true);
-      setHints(currGame.hints);
-    }
-  };
-
-  const handleGameReset = () => {
-    currGame.score = 0;
-    currGame.lives = 3;
-    currGame.hints = 5;
-    currGame.dictionary = new Map();
-
-    setScore(currGame.score);
-    setLives(currGame.lives);
-    setHints(currGame.hints);
-
-    const newWord = generate({ maxLength: 2 }) as string;
-    setWord(newWord);
-
-    setActiveGame(true);
-  };
-
-  const handleButtonClose = () => {
-    setViewHints(false);
-  };
-
-  const handleHome = () => {
-    setActiveGame(true);
-    setHome(true);
-  }
-
+  // if game is over/not an active game
   if (!activeGame) {
     return (
       <div>
         <Score score={score} />
         <h1>Game Over!</h1>
-        <GameReset onClick={handleGameReset} />
-        <ButtonHome onClick={handleHome} />
+        <GameReset
+          onClick={() =>
+            handleGameReset(
+              setNumScore,
+              setNumLives,
+              setNumHints,
+              setWord,
+              setActiveGame
+            )
+          }
+        />
+        <ButtonHome onClick={() => handleClickHome(setActiveGame, setHome)} />
       </div>
     );
   }
 
+  // When view hint is used
   if (viewHints) {
     return (
       <div>
@@ -146,7 +81,7 @@ function App() {
             </li>
           ))}
         </ul>
-        <ButtonClose onClick={handleButtonClose} />
+        <ButtonClose onClick={() => handleClickCloseDictionary(setViewHints)} />
       </div>
     );
   }
@@ -156,8 +91,12 @@ function App() {
     return (
       <div>
         <h1>Memory Game</h1>
-        <ButtonStartNorm onClick={handleClickStartNorm} />
-        <ButtonStartHard onClick={handleClickStartHard} />
+        <ButtonStartNorm onClick={() => handleClickStartNorm(setHome)} />
+        <ButtonStartHard
+          onClick={() =>
+            handleClickStartHard(setNumLives, setHome, setHardMode)
+          }
+        />
       </div>
     );
   } else if (hardMode) {
@@ -167,8 +106,28 @@ function App() {
         <Word word={word} />
         <Score score={score} />
         <Lives lives={lives} />
-        <ButtonAdd onClick={handleClickAdd} />
-        <ButtonRemove onClick={handleClickRemove} />
+        <ButtonAdd
+          onClick={() =>
+            handleClickAdd(
+              word,
+              setNumScore,
+              setNumLives,
+              setActiveGame,
+              setWord
+            )
+          }
+        />
+        <ButtonRemove
+          onClick={() =>
+            handleClickRemove(
+              word,
+              setNumScore,
+              setNumLives,
+              setActiveGame,
+              setWord
+            )
+          }
+        />
       </div>
     );
   } else {
@@ -179,12 +138,32 @@ function App() {
         <Score score={score} />
         <Lives lives={lives} />
         <Hints hints={hints} />
-        <ButtonAdd onClick={handleClickAdd} />
-        <ButtonRemove onClick={handleClickRemove} />
-        <ButtonHint onClick={handleClickHint} />
+        <ButtonAdd
+          onClick={() =>
+            handleClickAdd(
+              word,
+              setNumScore,
+              setNumLives,
+              setActiveGame,
+              setWord
+            )
+          }
+        />
+        <ButtonRemove
+          onClick={() =>
+            handleClickRemove(
+              word,
+              setNumScore,
+              setNumLives,
+              setActiveGame,
+              setWord
+            )
+          }
+        />
+        <ButtonHint
+          onClick={() => handleClickHint(setViewHints, setNumHints)}
+        />
       </div>
     );
   }
 }
-
-export default App;
